cd7e0c710bc6946edde464b99b4e39f37da668f6
Anshuman <chaurasiaji200@gmail.com>
2023-08-22 00:09:07 +0530

    Bug fixes
    The positioning of signs was affecting the way calculator handles
    multiple sign button clicks.

--- script.js
+++ script.js
@@ -11,74 +11,108 @@ let displayContent = '';  // to store content of clicked button
 
 inputBtn.forEach((btn) => {
 
-
-
-
     btn.addEventListener('click', () => {
-        
+                   
         if (display.firstChild === dummy) {
             display.removeChild(dummy);
         }
 
         if (btn.classList.contains('sign')) {
+
+                    // debugger;
+            if (attempts === 0) {
+               
+                firstNum = displayContent;
+                sign = btn.id;
+                displayContent = '';
+                attempts++;
+                display.innerText = firstNum + btn.value;
+
             
-            
-            firstNum = displayContent;
-            sign = btn.id;
-            displayContent = '';
-            
-            
-           
+            }
+
+            else {
+
+                if (displayContent !== '') {
+                    firstNum = operate(firstNum, displayContent, sign);
+                    sign = btn.id;
+                    display.innerText = firstNum + btn.value;
+                    displayContent ='';
+                    
+                }
+                else {
+                    sign = btn.id;
+                    display.innerText = firstNum + btn.value;
+                    
+                  
+                }
+                             
+            }
+
+
 
         }
         else {
             displayContent += btn.value;
+            display.innerText += btn.value;
         }
 
-        display.innerText += btn.value;
         
-      
+
     })
 
 
 });
 
 
+equal.addEventListener('click', () => {
 
-equal.addEventListener('click', () =>{
-
-
-    secondNum = displayContent;
-    
-   display.textContent=     `${operate(sign)}`;
-   displayContent = operate(sign);
+    displayContent = operate(firstNum, displayContent, sign);
+    display.textContent = displayContent;
+    attempts = 0;
 
 });
 
-clearBtn.addEventListener('click', () => {
-       firstNum='';
-       secondNum='';
-       sign='';
-    displayContent = '';
-    display.innerText = '';
-    display.appendChild(dummy);
-
-});
+function operate(a, b, sign) {
 
 
-
-function operate(sign) {
+    b = Number(b);
+    a = Number(a);
+    let result;
 
     switch (sign) {
 
         case 'add':
-            return +firstNum + +secondNum;
+            result = a + b;
+            break;
         case 'subtract':
-            return +firstNum - +secondNum;
+            result = a - b;
+            break;
         case 'multiply':
-            return  +firstNum * +secondNum ;
+            result = a * b;
+            break;
         case 'divide':
-            return divide +firstNum === 0 ? 'NOT PERMITTED' : +firstNum / +secondNum;
+            result = b === 0 ? 'invalid' : a / b;
+            break;
+        default:
+            result = a;
     }
+
+    return result % 1 === 0 ? result : result.toFixed(2);
 }
+
+
+clearBtn.addEventListener('click', () => {
+    firstNum = '';
+    secondNum = '';
+    sign = '';
+    displayContent = '';
+    display.innerText = '';
+    display.appendChild(dummy);
+    attempts = 0;
+});
+
+
+
+
 
